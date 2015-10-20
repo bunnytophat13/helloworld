8bc64bf91c339082a50679e875dbd43c629352ba
Kelly <poppy22132@gmail.com>
2015-10-20 13:01:01 -0500

    we granted the program the ability to change windows!!!!!!!!!!!

--- app/src/main/java/com/example/jchuah/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/jchuah/myapplication/Main2Activity.java
@@ -1,43 +1,25 @@
 package com.example.jchuah.myapplication;
 
-import android.content.Intent;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
-    static String tag = "com.example.jchuah.myapplication.MainActivity";
-
-
-            Bundle sendBundle = new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void onNextClick(View source){
-        //Toast.makeText(this, "Howdy", Toast.LENGTH_LONG ).show();
-        Log.d("LOLOLOL", "Next button clicked");
-        Log.i(tag, "Launching Color Activity");
-        Intent colorActivityIntent = new Intent (this, Main2Activity.class);
-
-        sendBundle.putString("name", "Tom");
-
-        colorActivityIntent.putExtra("groceries", sendBundle);
-
-        startActivity (colorActivityIntent, sendBundle);
+        setContentView(R.layout.activity_main2);
+        Bundle groceries = getIntent().getBundleExtra("groceries");
+        Log.i("GROCERIES!", groceries.getString("name"));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
